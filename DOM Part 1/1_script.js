cc5e3360a09a219c38d4459a75931b4c4eff8f90
Pra-sha-nta <parajuliprashant9@gmail.com>
2026-02-03 16:17:57 +0545

    Upto DOM

---
+++ DOM Part 1/1_script.js
@@ -0,0 +1,30 @@
+// console.log("DOM Part 1");
+// alert("DOM Part 1");
+// console.dir(document);
+// console.log(document);
+// console.dir(document.body);
+// document.body.style.backgroundColor = "magenta";
+
+// //selecting elements
+// console.log(document.getElementById("heading"));
+// console.log(document.getElementsByClassName("Class_name"));
+// console.log(document.getElementsByTagName("h3"));
+// console.log(document.querySelector("h3"));
+// console.log(document.querySelectorAll("h3"));
+
+// console.log(document.querySelector("#heading"));
+// console.log(document.querySelectorAll(".Class_name"));
+
+// #......... Dom Manipulation Properties .........#
+
+// 1. tagNAme : returns tag for element nodes
+
+// console.log(document.body.tagName);
+let childern = document.querySelector("div").childern
+console.log(childern);
+// 2. innerText : returns the text content of the element and all its child elements
+
+
+// 3. innerHTML : returns the plain text or  HTML content of the element
+
+// 4. textContent : returns the text content even for hidden element
